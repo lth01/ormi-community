da005f710637b86229618e24b363eab9c4a028a0
lth01 <dlxogml11235@naver.com>
2024-03-27 09:15:35 +0900

    Feat: 레이아웃 컴포넌트 "Header" 개발
    1. 로고 svg 컴포넌트 추가
    2. Menu 컴포넌트 clickCallback전 수행되는 beforeCallback추가
    3. 로그인 여부따른 레이아웃 변경 테스트
    
    Resolve: #14

--- src/main/front/ormi-community/src/components/Menu/Menu.jsx
+++ src/main/front/ormi-community/src/components/Menu/Menu.jsx
@@ -1,24 +1,32 @@
 import HeartIcon from "../Icon/HeartIcon";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GenerateLiElUUID } from "../utils/keygenerator"
 
 // 게시판 목록 메뉴를 제외한 일반적인 메뉴를 표현
-function Menu({href, svg, children, size}){
+function Menu({href, svg, children, beforeClicks, size}){
     const defaultIcon = HeartIcon(getSizeToSvgClassName(size));
+    const [onclicks, setOnClick] = useState([]);
     const [svgIconEl, setSvgIcon] = useState(svg ? svg(getSizeToSvgClassName(size)) : defaultIcon);
+    const moveToHref = () =>{
+        location.href = href;
+    };
 
     const changeSvgIcon = (newSvg) =>{
         setSvgIcon(newSvg ? newSvg : defaultIcon);
     }
 
-    const moveToHref = () =>{
-        location.href = href;
-    };
-
+    useEffect(() => {
+        setOnClick(beforeClicks.length > 0 ? [...beforeClicks, moveToHref ] : [moveToHref]);
+    },[]);
+   
     return (
-        <li key={GenerateLiElUUID()} className="list-none flex items-center gap-2 text-sm font-medium truncate" onClick={moveToHref}>
-            {svgIconEl}
-            <span className="trunacte">
+        <li key={GenerateLiElUUID()}
+            className="list-none flex items-center gap-2 text-sm font-medium truncate"
+            onClick={() => {
+                onclicks.forEach(onclick => onclick.call());
+        }}>
+                {svgIconEl}
+                <span className="trunacte">
                 {children}
             </span>
         </li>
